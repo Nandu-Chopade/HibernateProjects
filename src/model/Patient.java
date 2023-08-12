c3d336beb76e7e8fec8439033b3373f846ee2d2a
Nandu-Chopade <nanduchopade1998@gmail.com>
2023-08-12 19:23:58 +0530

    This is OneToOne relationship project in hibernate

---
+++ src/model/Patient.java
@@ -0,0 +1,53 @@
+package model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int age;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	private Address address;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Petient [id=" + id + ", name=" + name + ", age=" + age + ", address=" + address + "]";
+	}
+	
+	
+}
